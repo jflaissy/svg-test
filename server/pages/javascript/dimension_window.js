2a678ea4ddb5af79b4e5c201fecab73de0ab2c6c
Pierre Besson <pierrebesson@macbook-pro-de-pierre-besson.local>
2010-05-29 14:37:57 +0200

    J'ai fais en sorte d'avoir un fond rouge et que l'image soit au ccentre de la fenetre que j'ai agrangit. Je passe à Selenium et sa doc  maintenant.

--- server/pages/javascript/dimension_window.js
+++ server/pages/javascript/dimension_window.js
@@ -1,8 +1,13 @@
-window.onload=initialize();
+//window.onload=initialize();
 
 function initialize(){
-	
-	window.innerHeight=700;
-	window.innerWidth=700;
-	
+	var hauteur = 700;
+	var largeur=1000; 
+	window.innerHeight=hauteur;
+	window.innerWidth=largeur;
+	var largeur_image=document.getElementById('cadre').offsetWidth;
+	var hauteur_image=document.getElementById('cadre').offsetHeight;
+	document.getElementById('cadre').style.left=(largeur/2-largeur_image/2)+"px";
+	document.getElementById('cadre').style.top=(hauteur/2-hauteur_image/2)+"px";
+
 	}
