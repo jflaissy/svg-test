635883892679a5614ddc12fdba9038f48a289aa0
jflaissy <julien.flaissy@gmail.com>
2010-06-07 19:47:37 +0200

    Refonte du squelette pour refléter les modifications de la réunion de ce lundi.

---
+++ squelette/pretraitement/vide.py
@@ -0,0 +1,8 @@
+# un pretraitement prend un fichier svg et en fait un autre il modifie
+# le champ source de la structure.
+
+# cf. remarques posttraitement/
+
+def go(s):
+    print 'Pretraitement vide.'
+    s.pretraitement = True
