d6f080cfff1b81bfb14eaa1efbb3480023cb15a3
Pierre Besson <pierrebesson@dhcpwifi-23-181.enst.fr>
2010-06-09 09:37:54 +0200

    Un petit ajout sur le xml, il manquait la source.

--- pierre/parser.py
+++ pierre/parser.py
@@ -4,22 +4,28 @@ class PageMaker(ContentHandler):
     passthrough = False 
     def startElement(self, name, attrs):
         if name == 'tests': 
-            self.passthrough = True 
-            self.out = open(name + '.py', 'w') 
-            self.out.write(name+'[] \n')
+            self.passthrough = True
+            self.tests=[]
             
         if name == 'test': 
             self.test= True
-            self.out.write(tests.append())
+            self.testcourant={}
+            
+        if name=='type'
+            self.testcourant.[name] = name #pour ajouter une valeur dans le tableau associtif
+            
+            
                 
     def endElement(self, name): 
         if name == 'tests':
             self.passthrough = False  
-            self.out.close()
+            
         if name == 'test': 
             self.test= False    
+            self.tests.append(test_courant)
                
     def characters(self, chars): 
         if self.passthrough: self.out.write(chars)
+
 
 parse('parser.xml', PageMaker ())
