70c3f978423a3ae53c9968b6e2d7e6c564cb8ed3
jflaissy <julien.flaissy@gmail.com>
2010-06-24 10:03:32 +0200

    (mineur) debug.

--- squelette/pretraitement/remove_text.py
+++ squelette/pretraitement/remove_text.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 
 def go(input_file, output_file, parameters=None):
+     print 'Pretraitement remove_text. in:', input_file, 'out:', output_file
      # On lance le programme xsltproc sur la feuille de style dans xslt/.
      xsl_path = os.path.join('xslt', 'svg_filter_text.xsl')
      p = subprocess.Popen(['xsltproc', '-o', output_file, xsl_path, input_file])
